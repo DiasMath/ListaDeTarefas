b6dacfe7369bf7a9468e9152e90e9c97143a2770
DiasMath <matheusdiassandes@outlook.com>
2024-03-26 17:20:50 -0300

    first commit

---
+++ app/src/main/java/com/diasmath/listadetarefascompose/view/SalvarTarefa.kt
@@ -0,0 +1,12 @@
+package com.diasmath.listadetarefascompose.view
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+
+fun SalvarTarefa(
+    navController: NavController
+){
+
+}
